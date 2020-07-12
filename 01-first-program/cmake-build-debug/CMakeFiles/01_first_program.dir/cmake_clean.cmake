3eb2d779768638ce8a9238c8fcff46946e327f2c
Ethan Yu <sohbmpethan@gmail.com>
2020-07-12 15:50:04 +0900

    01-first-program

---
+++ 01-first-program/cmake-build-debug/CMakeFiles/01_first_program.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "01_first_program"
+  "01_first_program.pdb"
+  "CMakeFiles/01_first_program.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/01_first_program.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
